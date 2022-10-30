90737befc7929720b812e0a00930123da4adeb5c
Buzz2d0 <admin@imipy.com>
2022-10-30 15:34:53 +0800

    xun syscall table

--- shellcode/helloworld.asm
+++ shellcode/helloworld.asm
@@ -19,6 +19,7 @@
 ;; macOS 已将系统调用号分成几个不同的“类别”，系统调用号的高位代表系统调用的类，在当前例子中 wirte、exit 属于 Unix/BSD 因此高位是 2 每个 Unix 系统调用都是(0×2000000 + unix syscall)
 
 ;; 参考文章：http://dustin.schultz.io/mac-os-x-64-bit-assembly-system-calls.html
+;; syscall table: https://opensource.apple.com/source/xnu/xnu-1504.3.12/bsd/kern/syscalls.master
 
 BITS    64
 global  _main
